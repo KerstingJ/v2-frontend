c7a65491ba81d447bf4c29979c3b9aa67f2b54e9
Josh <Kersting.Josh@Gmail.com>
2019-07-26 22:39:42 -0400

    added react-redux, fixed app.js incomplete tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Route } from "react-router-dom"
+import { Route } from "react-router-dom";
 
 // import PrivateRoute from './components/PrivateRouter';
-import Main from "./views/Main"
+import Main from "./views/Main";
 
 export default function(props) {
-  return (<div className="App">
-    <Route to="/" component={Main}
-  </div>);
+  return (
+    <div className="App">
+      <Route to="/" component={Main} />
+    </div>
+  );
 }
