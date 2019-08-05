81d352e462b6edf5d339df6079854a693c714eda
Josh <Kersting.Josh@Gmail.com>
2019-08-04 23:31:12 -0400

    changes to iframe styles

--- src/views/Resume.js
+++ src/views/Resume.js
@@ -43,6 +43,13 @@ const Main = styled.main`
       flex-direction: column;
     }
 
+    iframe {
+      width: 100%;
+      height: 100vh;
+      margin: 0px;
+      padding: 0px;
+    }
+
     .con-block {
       width: 49%;
 
